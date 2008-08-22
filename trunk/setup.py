5acf227906abfa852a762fa8cde0c1f891993342
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-08-22 20:38:30 +0000

    Reorganized and ready for release

---
+++ trunk/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = "rst2pdf",
+    version = "0.2",
+    packages = ["rst2pdf"],
+    install_requires = ['docutils>=0.3','reportlab>=2.1'],
+    # metadata for upload to PyPI
+    author = "Roberto Alsina",
+    author_email = "ralsina@netmanagers.com.ar",
+    description = "Convert restructured text to PDF via reportlab.",
+    license = "MIT",
+    keywords = "rst pdf docutils reportlab",
+    url = "http://rst2pdf.googlecode.com",   # project home page, if any
+    download_url = "http://code.google.com/p/rst2pdf/downloads/list",
+    entry_points = {'console_scripts': ['rst2pdf = rst2pdf.rst2pdf:main']},
+
+)
