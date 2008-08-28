d9555b4382228d58e3d761f887476135c7bbc9c2
webmaster@guichen.com <webmaster@guichen.com@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-08-28 16:21:05 +0000

    refactoring tests

--- rst2pdf/tests/test_rst2pdf.py
+++ rst2pdf/tests/test_rst2pdf.py
@@ -2,24 +2,22 @@
 import unittest
 from pyPdf import PdfFileReader
 import cStringIO
+import rst2pdf
 from rst2pdf.createpdf import createPdf, gen_elements
+from rst2pdf import styles as sty
 
 import docutils.core
 
 from os.path import join, abspath, dirname, basename
 PREFIX = abspath(dirname(__file__))
 
+import pdb
+
+
 def input_file_path(file):
     return join(PREFIX, 'input', file)
 
-class GenerationTests(unittest.TestCase):
-
-    def setUp(self):
-        pass
-        
-    def tearDown(self):
-        pass
-        
+class FullGenerationTests(unittest.TestCase):
     def test_bullet_chars_full(self):
         self.output=cStringIO.StringIO()
         input_file = input_file_path('test_bullet_chars.txt')
@@ -29,12 +27,24 @@ class GenerationTests(unittest.TestCase):
         result = PdfFileReader(self.output)
         text_result = result.getPage(0).extractText()
         self.assertEqual(text_result, u'\nTest\nItem 1\nItem 2\n')
+
+    
+class GenerationTests(unittest.TestCase):
+
+    def setUp(self):
+        defSsheet= join(rst2pdf.__path__[0], 'styles.json')
+        self.styles=sty.getStyleSheet(defSsheet)
+        
+    def tearDown(self):
+        pass
+        
         
     def test_bullet_chars(self):
         input_file = input_file_path('test_bullet_chars.txt')
         input=open(input_file,'r').read()
         doctree=docutils.core.publish_doctree(input)
-        elements=gen_elements(doctree,0)
+        elements=gen_elements(doctree,0, style=self.styles)
+        pdb.set_trace()
         
 def test_suite():
     return unittest.makeSuite(GenerationTests)
