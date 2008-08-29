bd97eef73c3f028c9933e5521f0d9e8e02e4c971
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-08-29 20:04:21 +0000

    Merged multiple stylesheets branch.
    If I knew it was going to be this easy I wouln't have branched it ;-)

--- trunk/rst2pdf/styles.py
+++ trunk/rst2pdf/styles.py
@@ -3,6 +3,8 @@
 #$LastChangedDate$
 #$LastChangedRevision$
 
+import os
+import sys
 import re
 from reportlab.platypus import *
 import reportlab.lib.colors as colors
@@ -12,10 +14,9 @@ from reportlab.lib.fonts import addMapping
 from reportlab.lib.styles import *
 from reportlab.lib.enums import *
 from reportlab.pdfbase import pdfmetrics
-
-
-import os,sys
+import reportlab.lib.pagesizes as pagesizes
 from utils import log
+from simplejson import loads
 
 try:
     from wordaxe.rl.paragraph import Paragraph
@@ -23,17 +24,6 @@ try:
 except:
     log.warning("No hyphenation support, install wordaxe")
 
-
-
-
-# Set these, and then **maybe** think of setting the stylesheets below
-# if you want finer control
-
-stdFont       = 'Helvetica'
-stdBold       = 'Helvetica-Bold'
-stdItalic     = 'Helvetica-Oblique'
-stdBoldItalic = 'Helvetica-BoldOblique'
-stdMono       = 'Courier'
 
 def adjustUnits(v,total=0):
     '''Takes something like 2cm and returns 2*cm.
@@ -56,153 +46,174 @@ def adjustUnits(v,total=0):
       log.error('Unknown unit "%s"' % u)
       return float(n)
 
-TTFSearchPath=[os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fonts'),'.']
+class StyleSheet(object):
+    '''Class to handle a collection of stylesheets'''
+    def __init__(self,flist):
+        log.info('Using stylesheets: %s'%','.join(flist))
+        '''flist is a list of stylesheet filenames.They will
+        be loaded and merged in order'''
+        self.TTFSearchPath=[os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fonts'),'.']
 
-def findFont(fn):
-    if not os.path.isabs(fn):
-        for D in TTFSearchPath:
-            tfn = os.path.join(D,fn)
-            if os.path.isfile(tfn):
-                return str(tfn)
-    return str(fn)
+        # Page width, height
+        self.pw=0
+        self.ph=0
 
-# Page width, height
-pw=0
-ph=0
+        # Page size [w,h]
+        self.ps=None
 
-# Page size [w,h]
-ps=None
+        # Margins (top,bottom,left,right,gutter)
+        self.tm=0
+        self.bm=0
+        self.lm=0
+        self.rm=0
+        self.gm=0
 
-# Margins (top,bottom,left,right)
-tm=0
-bm=0
-lm=0
-rm=0
-gm=0
+        #text width
+        self.tw=0
 
-#text width
-tw=0
-
-languages=[]
-
-import reportlab.lib.pagesizes as pagesizes
-
-def getStyleSheet(fname):
-    """Returns a stylesheet object"""
-    global pw,ph,ps,tm,bm,lm,rm,tw,gm,languages
-    stylesheet = StyleSheet1()
-
-    from simplejson import loads
-
-    try:
-        data=loads(open(fname).read())
-    except ValueError,e: # Error parsing the JSON data
-        log.error('Error parsing stylesheet "%s": %s'%(fname,str(e)))
-        sys.exit(1)
-    except IOError,e: #Error opening the ssheet
-        log.error('Error opening stylesheet "%s": %s'%(fname,str(e)))
-        sys.exit(1)
-    
-        
-    styles=data['styles']
-    fonts=data['fontsAlias']
-    page=data['pageSetup']
-    if page['size']: # A standard size
-        if page['size'] in pagesizes.__dict__:
-            ps=pagesizes.__dict__[page['size']]
-        else:
-            log.critical('Unknown page size %s' % page['size'])
-            sys.exit(1)
-    else: #A custom size
-        # The sizes are expressed in some unit.
-        # For example, 2cm is 2 centimeters, and we need
-        # to do 2*cm (cm comes from reportlab.lib.units)
-        ps=[adjustUnits(page['width']),adjustUnits(page['height'])]
-    pw,ph=ps
-    lm=adjustUnits(page['margin-left'])
-    rm=adjustUnits(page['margin-right'])
-    tm=adjustUnits(page['margin-top'])
-    bm=adjustUnits(page['margin-bottom'])
-    gm=adjustUnits(page['margin-gutter'])
-
-
-    # tw is the text width.
-    # We need it to calculate header-footer height
-    # and compress literal blocks.
-    tw=pw-lm-rm-gm
-    embedded=data['embeddedFonts']
-
-    for font in embedded:
-        try:
-            # Each "font" is a list of four files, which will be used for
-            # regular / bold / italic / bold+italic versions of the font.
-            # If your font doesn't have one of them, just repeat the regular
-            # font.
-
-            # Example, using the Tuffy font from http://tulrich.com/fonts/
-            # "embeddedFonts" : [
-            #                    ["Tuffy.ttf","Tuffy_Bold.ttf","Tuffy_Italic.ttf","Tuffy_Bold_Italic.ttf"]
-            #                   ],
-
-            # The fonts will be registered with the file name, minus the extension.
-
-            for variant in font:
-                pdfmetrics.registerFont(TTFont(str(variant.split('.')[0]), findFont(variant)))
-
-                # And map them all together
-                regular,bold,italic,bolditalic = [ variant.split('.')[0] for variant in font ]
-                addMapping(regular,0,0,regular)
-                addMapping(regular,0,1,italic)
-                addMapping(regular,1,0,bold)
-                addMapping(regular,1,1,bolditalic)
-        except:
-             try:
-                fname=font[0].split('.')[0]
-                log.error("Error processing font %s, registering as Helvetica alias",fname)
-                fonts[fname]='Helvetica'
-             except:
-                log.error("Error processing font %s",fname)
+        self.languages=[]
+        ssdata=[]
+        # First parse all files and store the results
+        for fname in flist:
+            try:
+                ssdata.append(loads(open(fname).read()))
+            except ValueError,e: # Error parsing the JSON data
+                log.error('Error parsing stylesheet "%s": %s'%(fname,str(e)))
+                sys.exit(1)
+            except IOError,e: #Error opening the ssheet
+                log.error('Error opening stylesheet "%s": %s'%(fname,str(e)))
                 sys.exit(1)
 
-    while True:
-        for [skey,style] in data['styles']:
-            sdict={}
-            if 'parent' in style:
-                if not style['parent']:
-                    del style['parent']
+        # Get pageSetup data from all stylessheets in order:
+        for data,ssname in zip(ssdata,flist):
+            page=data.get('pageSetup',{})
+            if page.get('size',None): # A standard size
+                if page['size'] in pagesizes.__dict__:
+                    self.ps=pagesizes.__dict__[page['size']]
                 else:
-                    style['parent']=stylesheet[style['parent']]
-            # FIXME: this is done completely backwards
-            for key in style:
-                # Handle font aliases
-                if key in ['fontName','bulletFontName'] and style[key] in fonts:
-                    style[key]=fonts[style[key]]
+                    log.critical('Unknown page size %s in stylesheet %s' % (page['size'],ssname))
+                    sys.exit(1)
+            else: #A custom size
+                # The sizes are expressed in some unit.
+                # For example, 2cm is 2 centimeters, and we need
+                # to do 2*cm (cm comes from reportlab.lib.units)
+                self.ps=[adjustUnits(page['width']),adjustUnits(page['height'])]
+            self.pw,self.ph=self.ps
+            if 'margin-left' in page:
+                self.lm=adjustUnits(page['margin-left'])
+            if 'margin-right' in page:
+                self.rm=adjustUnits(page['margin-right'])
+            if 'margin-top' in page:
+                self.tm=adjustUnits(page['margin-top'])
+            if 'margin-bottom' in page:
+                self.bm=adjustUnits(page['margin-bottom'])
+            if 'margin-butter' in page:
+                self.gm=adjustUnits(page['margin-gutter'])
 
-                # Handle color references by name
-                elif key in ['backColor','textColor'] and style[key]:
-                    if style[key] in colors.__dict__:
-                        style[key]=colors.__dict__[style[key]]
-                    else: # Hopefully, a hex color:
-                        c=style[key]
-                        r=int(c[1:3],16)
-                        g=int(c[3:5],16)
-                        b=int(c[5:7],16)
-                        style[key]=colors.Color(r,g,b)
+            # tw is the text width.
+            # We need it to calculate header-footer height
+            # and compress literal blocks.
+            self.tw=self.pw-self.lm-self.rm-self.gm
 
-                # Handle alignment constants
-                elif key == 'alignment':
-                    style[key]={'TA_LEFT':0, 'TA_CENTER':1, 'TA_CENTRE':1, 'TA_RIGHT':2, 'TA_JUSTIFY':4}[style[key]]
+        # Get font aliases from all stylesheets in order
+        self.fonts={}
+        for data,ssname in zip(ssdata,flist):
+            self.fonts.update(data.get('fontsAlias',{}))
+            embedded=data.get('embeddedFonts',[])
 
-                elif key == 'language':
-                    if not style[key] in languages:
-                        languages.append(style[key])
-                # Make keys str instead of unicode (required by reportlab)
-                sdict[str(key)]=style[key]
-                sdict['name']=skey
-            stylesheet.add(ParagraphStyle(**sdict))
-        else:
-            break
-    return stylesheet
+            for font in embedded:
+                try:
+                    # Each "font" is a list of four files, which will be used for
+                    # regular / bold / italic / bold+italic versions of the font.
+                    # If your font doesn't have one of them, just repeat the regular
+                    # font.
+
+                    # Example, using the Tuffy font from http://tulrich.com/fonts/
+                    # "embeddedFonts" : [
+                    #                    ["Tuffy.ttf","Tuffy_Bold.ttf","Tuffy_Italic.ttf","Tuffy_Bold_Italic.ttf"]
+                    #                   ],
+
+                    # The fonts will be registered with the file name, minus the extension.
+
+                    for variant in font:
+                        pdfmetrics.registerFont(TTFont(str(variant.split('.')[0]), findFont(variant)))
+
+                        # And map them all together
+                        regular,bold,italic,bolditalic = [ variant.split('.')[0] for variant in font ]
+                        addMapping(regular,0,0,regular)
+                        addMapping(regular,0,1,italic)
+                        addMapping(regular,1,0,bold)
+                        addMapping(regular,1,1,bolditalic)
+                except:
+                    try:
+                        fname=font[0].split('.')[0]
+                        log.error("Error processing font %s, registering as Helvetica alias",fname)
+                        fonts[fname]='Helvetica'
+                    except:
+                        log.error("Error processing font %s",fname)
+                        sys.exit(1)
+
+        # Get styles from all stylesheets in order
+        self.stylesheet = {}
+        self.styles=[]
+        for data,ssname in zip(ssdata,flist):
+            styles=data.get('styles',{})
+            for [skey,style] in styles:
+                sdict={}
+                # FIXME: this is done completely backwards
+                for key in style:
+                    # Handle font aliases
+                    if key in ['fontName','bulletFontName'] and style[key] in self.fonts:
+                        style[key]=self.fonts[style[key]]
+
+                    # Handle color references by name
+                    elif key in ['backColor','textColor'] and style[key]:
+                        if style[key] in colors.__dict__:
+                            style[key]=colors.__dict__[style[key]]
+                        else: # Hopefully, a hex color:
+                            c=style[key]
+                            r=int(c[1:3],16)
+                            g=int(c[3:5],16)
+                            b=int(c[5:7],16)
+                            style[key]=colors.Color(r,g,b)
+
+                    # Handle alignment constants
+                    elif key == 'alignment':
+                        style[key]={'TA_LEFT':0, 'TA_CENTER':1, 'TA_CENTRE':1, 'TA_RIGHT':2, 'TA_JUSTIFY':4}[style[key]]
+
+                    elif key == 'language':
+                        if not style[key] in self.languages:
+                            self.languages.append(style[key])
+                    # Make keys str instead of unicode (required by reportlab)
+                    sdict[str(key)]=style[key]
+                    sdict['name']=skey
+                # If the style already exists, update it
+                if skey in self.stylesheet:
+                    self.stylesheet[skey].update(sdict)
+                else: # New style
+                    self.stylesheet[skey]=sdict
+                    self.styles.append(sdict)
+        # And create  reportlabs stylesheet
+        self.StyleSheet=StyleSheet1()
+        for s in self.styles:
+            if 'parent' in s:
+                if not s['parent']:
+                    del s['parent']
+                else:
+                    s['parent']=self.StyleSheet[s['parent']]
+            self.StyleSheet.add(ParagraphStyle(**s))
+    def __getitem__(self,key):
+        return self.StyleSheet[key]
+
+    def findFont(fn):
+        '''Given a font file name, searches for it in TTFSearchPath
+        and returns the real file name'''
+        if not os.path.isabs(fn):
+            for D in self.TTFSearchPath:
+                tfn = os.path.join(D,fn)
+                if os.path.isfile(tfn):
+                    return str(tfn)
+        return str(fn)
 
 
 
