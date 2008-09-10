eb6aee59db313a32158b76943be7c897837c3bc0
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-09-10 14:50:50 +0000

    This Writer class is almost good enough to make Sphinx work.

--- trunk/rst2pdf/writer.py
+++ trunk/rst2pdf/writer.py
@@ -1,15 +1,18 @@
-"""
-Simple pdf Writer, writes a PDF using reportlab
-"""
+# -*- coding: utf-8 -*-
 
-__docformat__ = 'reStructuredText'
-
-
-import createpdf
+from rst2pdf import createpdf
 from docutils import writers
 from StringIO import StringIO
+import sys
+from tempfile import mkstemp
+import codecs
 
-class Writer(writers.Writer):
+class PdfWriter(writers.Writer):
+
+    def __init__(self, builder):
+        writers.Writer.__init__(self)
+        self.builder = builder
+        self.output = u''
 
     supported = ('pdf')
     """Formats this writer supports."""
@@ -17,22 +20,13 @@ class Writer(writers.Writer):
     config_section = 'pdf writer'
     config_section_dependencies = ('writers',)
 
-    output = None
     """Final translated form of `document`."""
 
     def translate(self):
-        sio=StringIO()
-        createpdf.createPdf(doctree=self.document,output=sio)        
-        self.output=sio.getvalue()
+        sio=StringIO('')
+        createpdf.RstToPdf().createPdf(doctree=self.document,output=sio,compressed=False)
+        self.output=unicode(sio.getvalue(),'utf-8','ignore')
 
     def supports(self, format):
         """This writer supports all format-specific elements."""
         return 1
-
-if __name__ == "__main__":
-    from docutils.core import publish_cmdline, default_description
-
-    description = ('Generates PDF from standalone reStructuredText '
-                   'sources (for testing purposes).  ' + default_description)
-
-    publish_cmdline(writer=Writer(),description=description)
