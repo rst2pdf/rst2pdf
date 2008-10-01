94e2cf23bf471a2d25e4baca1844b4ab2afc385e
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-10-01 13:18:08 +0000

    Make it "work" when matplotlib is not available (issue 77)

--- rst2pdf/math_flowable.py
+++ rst2pdf/math_flowable.py
@@ -16,11 +16,11 @@ fonts={}
 
 class Math(Flowable):
     def __init__(self,s):
+        self.s=s
         if HAS_MATPLOTLIB:
-            self.s=s
             self.parser=mathtext.MathTextParser("Pdf")
         else:
-            log.error("Math support not available, install matplotlib")
+            log.error("Math support not available, some parts of this document will be rendered incorrectly. Install matplotlib.")
         Flowable.__init__(self)
       
     def wrap(self,aW,aH):
@@ -28,7 +28,7 @@ class Math(Flowable):
             width, height, descent, glyphs, rects, used_characters = \
             self.parser.parse('$%s$'%self.s, 72)
             return width, height
-        return 0,0
+        return 10,10
 
     def drawOn(self,canv,x,y,_sW=0):
         if HAS_MATPLOTLIB:
@@ -50,40 +50,45 @@ class Math(Flowable):
                 canv.rect(ox, oy+2*height, width, height,fill=1)
             canv.restoreState()
         else:
-            return
+            canv.saveState()
+            canv.drawString(x,y,self.s)
+            canv.restoreState()
 
     def descent(self):
         '''Returns the descent of this flowable, useful to align it when used inline'''
-        width, height, descent, glyphs, rects, used_characters = \
-        self.parser.parse('$%s$'%self.s, 72)
-        return descent
+        if HAS_MATPLOTLIB:
+            width, height, descent, glyphs, rects, used_characters = \
+            self.parser.parse('$%s$'%self.s, 72)
+            return descent
+        return 0
 
     def genImage(self):
         '''Create a PNG from the contents of this flowable. Required so we can
         put inline math in paragraphs. Returns the file name. The file
         is caller's responsability'''
-        if not HAS_MATPLOTLIB:
-            return None
         dpi=72
         scale=10
         import Image,ImageFont,ImageDraw,ImageColor
-        width, height, descent, glyphs, rects, used_characters = \
-        self.parser.parse('$%s$'%self.s,dpi)
-        img=Image.new('L',(int(width*scale),int(height*scale)),ImageColor.getcolor("white","L"))
-        draw=ImageDraw.Draw(img)
-        for ox, oy, fontname, fontsize, num, symbol_name in glyphs:
-            font=ImageFont.truetype(fontname,int(fontsize*scale))
-            tw,th=draw.textsize(unichr(num),font=font)
-            # No, I don't understand why that 4 is there. As we used to say in the pure math
-            # department, that was a numerical solution.
-            draw.text((ox*scale,(height-oy-fontsize+4)*scale),unichr(num),font=font)
+        if not HAS_MATPLOTLIB:
+            img=Image.new('L',(120,120),ImageColor.getcolor("black","L"))
+        else:
+            width, height, descent, glyphs, rects, used_characters = \
+            self.parser.parse('$%s$'%self.s,dpi)
+            img=Image.new('L',(int(width*scale),int(height*scale)),ImageColor.getcolor("white","L"))
+            draw=ImageDraw.Draw(img)
+            for ox, oy, fontname, fontsize, num, symbol_name in glyphs:
+                font=ImageFont.truetype(fontname,int(fontsize*scale))
+                tw,th=draw.textsize(unichr(num),font=font)
+                # No, I don't understand why that 4 is there. As we used to say in the pure math
+                # department, that was a numerical solution.
+                draw.text((ox*scale,(height-oy-fontsize+4)*scale),unichr(num),font=font)
 
-        for ox, oy, w, h in rects:
-            x1=ox*scale
-            x2=x1+w*scale
-            y1=(height-oy)*scale
-            y2=y1+h*scale
-            draw.rectangle([x1,y1,x2,y2],fill=ImageColor.getcolor("black","L"))
+            for ox, oy, w, h in rects:
+                x1=ox*scale
+                x2=x1+w*scale
+                y1=(height-oy)*scale
+                y2=y1+h*scale
+                draw.rectangle([x1,y1,x2,y2],fill=ImageColor.getcolor("black","L"))
         fh,fn=tempfile.mkstemp(suffix=".png")
         os.close(fh)
         img.save(fn)
