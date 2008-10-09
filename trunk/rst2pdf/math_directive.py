dafbe523507a3e63d9030575082a5e33213b8061
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-10-09 15:10:53 +0000

    Try to make the Math directive compatible with docutils 0.4

--- trunk/rst2pdf/math_directive.py
+++ trunk/rst2pdf/math_directive.py
@@ -4,15 +4,22 @@ from docutils.parsers import rst
 from docutils.parsers.rst import directives
 from docutils.nodes import General,Inline,Element
 from docutils.parsers.rst import roles
+import docutils
 
-class Math(rst.Directive):
-    has_content = True
-    def run(self):
-        # Raise an error if the directive does not have contents.
-        #self.assert_has_content()
-        return [ math_node(data=''.join(self.content),rawsource=''.join(self.content)) ]
-    def __repr__(self):
-        return u''.join(self.content)
+if 'Directive' in rst.__dict__:
+    class Math(rst.Directive):
+        has_content = True
+        def run(self):
+            return [ math_node(data=''.join(self.content),rawsource=''.join(self.content)) ]
+        def __repr__(self):
+            return u''.join(self.content)
+else:
+    def Math(name, arguments, options, content, lineno,
+            content_offset, block_text, state, state_machine):
+        return [ math_node(data=''.join(content),rawsource=''.join(content)) ]
+    Math.content=True
+    
+directives.register_directive('math', Math)
 
 class math_node(General,Inline,Element):
     children = ()
@@ -20,8 +27,6 @@ class math_node(General,Inline,Element):
        self.rawsource=rawsource
        self.math_data=attributes['data']
        Element.__init__(self,rawsource,*children, **attributes)
-
-directives.register_directive('math', Math)
 
 def math_role(name,rawtext,text,lineno,inliner,options={},content=[]):
     return [math_node(rawtext[7:-1],data=rawtext[7:-1])],[]
