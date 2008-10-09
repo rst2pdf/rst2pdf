3433e20ffecab5b095b65ce7f2d3c99fff823713
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2008-10-09 13:39:03 +0000

    Log to stderr only, small change to the format.

--- rst2pdf/log.py
+++ rst2pdf/log.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import logging,sys
-
+logging.basicConfig(format='[%(levelname)s] %(filename)s:%(lineno)d %(message)s',level=logging.WARNING)
 log = logging.getLogger('rst2pdf')
-hdlr = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('[%(levelname)s] %(filename)s L%(lineno)d %(message)s')
-hdlr.setFormatter(formatter)
-log.addHandler(hdlr)
-log.setLevel(logging.WARNING)
+#hdlr = logging.StreamHandler()
+#formatter = logging.Formatter('[%(levelname)s] %(filename)s:%(lineno)d %(message)s')
+#hdlr.setFormatter(formatter)
+#log.addHandler(hdlr)
+#log.setLevel(logging.WARNING)
