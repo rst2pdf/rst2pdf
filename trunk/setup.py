d46fc5091eccbfe85b022f5edf2ac9c338882fb9
cito@online.de <cito@online.de@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-01-23 01:47:44 +0000

    Possible solution for #84. The version is now set in only 1 place instead of 3, and the svn revision is added automatically. Removed the namespace package stuff since I don't see why this should be a namespace package and since a namespace package must not have any additional code in __init__.py.

--- trunk/setup.py
+++ trunk/setup.py
@@ -5,6 +5,8 @@
 import os
 from setuptools import setup, find_packages
 
+version = '0.9'
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
@@ -16,16 +18,16 @@ long_description = (
     + '\n' +
     read('README.txt')
     + '\n' +
-    'Contributors\n' 
+    'Contributors\n'
     '************\n'
 
-    
+
     + '\n' +
     read('Contributors.txt')
     + '\n' +
     'Change history\n'
     '**************\n'
-    + '\n' + 
+    + '\n' +
     read('CHANGES.txt')
     + '\n' +
    'Download\n'
@@ -40,18 +42,17 @@ install_requires = [
         'Pygments',
         'simplejson'
         ]
-        
+
 tests_require = ['pyPdf',]
 hyphenation_require = ['wordaxe',]
 svgsupport_require = ['uniconvertor',]
 sphinx_require = ['sphinx',]
 
 setup(
-    name = "rst2pdf",
-    version = "0.9",
+    name="rst2pdf",
+    version=version,
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     package_data = {'rst2pdf': ['*.json']},
-    namespace_packages=['rst2pdf'],
     include_package_data=True,
     # place to find an egg distrib of reportlab 2.1
     dependency_links = [
@@ -63,8 +64,7 @@ setup(
        "http://dist.repoze.org",
        # uniconvertor
        "http://sk1project.org/downloads/uniconvertor/v1.1.3/uniconvertor-1.1.3.tar.gz",
-       
-    ], 
+    ],
     install_requires = install_requires,
     tests_require=tests_require,
     extras_require=dict(tests = tests_require,
@@ -85,7 +85,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing',
         'Topic :: Utilities',
-    ], 
+    ],
     author = "Roberto Alsina",
     author_email = "ralsina at netmanagers dot com dot ar",
     description = "Convert restructured text to PDF via reportlab.",
