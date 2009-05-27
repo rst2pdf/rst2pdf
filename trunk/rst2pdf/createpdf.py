941a143832612f27a047b6dd19e611662124f9bc
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-05-27 00:40:12 +0000

    Make image sizing work like rst2latex: only use the image's DPI when using the natural size

--- trunk/rst2pdf/createpdf.py
+++ trunk/rst2pdf/createpdf.py
@@ -187,7 +187,6 @@ class RstToPdf(object):
         else:
             img = PILImage.open(imgname)
             iw, ih = img.size
-            xdpi, ydpi=img.info.get('dpi', (xdpi, ydpi))
 
         # Try to get the print resolution from the image itself via PIL.
         # If it fails, assume a DPI of 300, which is pretty much made up,
@@ -206,8 +205,9 @@ class RstToPdf(object):
             w = self.styles.adjustUnits(w, self.styles.tw,
                                         default_unit='px')
         else:
+            xdpi, ydpi=img.info.get('dpi', (xdpi, ydpi))
             log.warning("Using image %s without specifying size."
-                "Calculating based on %ddpi", imgname, xdpi)
+                "Calculating based on image size at %ddpi", imgname, xdpi)
             # No width specified at all, use w in px
             w = iw*inch/xdpi
 
