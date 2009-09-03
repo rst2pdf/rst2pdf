6791aa18c18fa9842e9721632651d9263e9d6254
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-03 13:30:15 +0000

    Try to provide more meaningful error messages

--- trunk/rst2pdf/log.py
+++ trunk/rst2pdf/log.py
@@ -9,3 +9,18 @@ logging.basicConfig(
     level=logging.WARNING)
 
 log = logging.getLogger('rst2pdf')
+
+def nodeid(node):
+    """Given a node, tries to return a way to see where it was in the 
+    source text"""
+    fname='UNKNOWN'
+    line='UNKNOWN'
+    try:
+        if node.line: line=str(node.line)
+    except:
+        pass
+    try:
+        if node.source: fname=str(node.source)
+    except:
+        pass
+    return 'near line %s in file %s'%(line,fname)
