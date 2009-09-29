4b8d40cfca5b789f21795d9abad529cb2ab6104e
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-29 16:11:55 +0000

    Stop generating useless diffs on unchanged files

--- trunk/gui/build.sh
+++ trunk/gui/build.sh
@@ -1,5 +1,9 @@
 
 #!/bin/sh
 
-for a in *.ui; do pyuic4 $a -o Ui_`basename $a .ui`.py -x; done
+for a in *.ui
+do 
+ 	sed -i 's/^# Created.*$//' $a
+	pyuic4 $a -o Ui_`basename $a .ui`.py -x
+done
 pyrcc4 icons.qrc -o icons_rc.py
