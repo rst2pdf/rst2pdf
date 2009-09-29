f5ef09fabddf408861688eae0fa8dc53b3ca1578
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-29 12:45:26 +0000

    Added background field for page template

--- gui/Ui_about.py
+++ gui/Ui_about.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'about.ui'
 #
-# Created: Tue Sep 29 09:32:47 2009
+# Created: Tue Sep 29 09:41:43 2009
 #      by: PyQt4 UI code generator 4.5.4
 #
 # WARNING! All changes made in this file will be lost!
