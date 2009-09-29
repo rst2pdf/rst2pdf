ba3e594ce06c2063b6885efc90dcc6ec10ad230a
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-29 16:22:37 +0000

    (lame) snippet output in page setup widget

--- trunk/gui/icons_rc.py
+++ trunk/gui/icons_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Tue Sep 29 12:51:06 2009
+# Created: Tue Sep 29 13:11:13 2009
 #      by: The Resource Compiler for PyQt (Qt v4.5.2)
 #
 # WARNING! All changes made in this file will be lost!
