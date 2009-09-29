8918137fdc0c70522a14cf7e28d87d53a800237c
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-29 16:14:41 +0000

    No more datestamps

--- gui/build.sh
+++ gui/build.sh
@@ -3,7 +3,7 @@
 
 for a in *.ui
 do 
- 	sed -i 's/^# Created.*$//' $a
 	pyuic4 $a -o Ui_`basename $a .ui`.py -x
+ 	sed -i 's/^# Created.*$//' Ui_`basename $a .ui`.py
 done
 pyrcc4 icons.qrc -o icons_rc.py
