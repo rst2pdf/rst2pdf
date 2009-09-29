9d3c34fb6d4af7ddbdbddad50510c4eb7b78fb92
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-29 12:36:02 +0000

    Complete the style widget

--- gui/Ui_about.py
+++ gui/Ui_about.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'about.ui'
 #
-# Created: Mon Sep 28 16:31:45 2009
+# Created: Tue Sep 29 09:32:47 2009
 #      by: PyQt4 UI code generator 4.5.4
 #
 # WARNING! All changes made in this file will be lost!
