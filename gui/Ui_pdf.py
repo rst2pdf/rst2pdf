20e41f485ba214edf17891f8fa3437bb51608151
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-09-29 13:35:58 +0000

    Hooked background field in page templates, highlight snippet

--- gui/Ui_pdf.py
+++ gui/Ui_pdf.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'pdf.ui'
 #
-# Created: Tue Sep 29 09:41:47 2009
+# Created: Tue Sep 29 10:27:10 2009
 #      by: PyQt4 UI code generator 4.5.4
 #
 # WARNING! All changes made in this file will be lost!
