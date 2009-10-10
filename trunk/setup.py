84212af66991f137fbd86fa39c12a041a1e8c61f
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-10-10 18:31:56 +0000

    Update to 0.12

--- trunk/setup.py
+++ trunk/setup.py
@@ -6,7 +6,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.11'
+version = '0.12.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
