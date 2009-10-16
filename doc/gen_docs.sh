0c2c6bf740f73ec5edffbbcdfd8a466fb0846ca8
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-10-16 10:54:24 +0000

    oops

--- doc/gen_docs.sh
+++ doc/gen_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python ../rst2pdf/createpdf manual.txt -o manual.pdf -s manual.style -b1
+python ../rst2pdf/createpdf.py manual.txt -o manual.pdf -s manual.style -b1
 rst2man rst2pdf.txt rst2pdf.1
