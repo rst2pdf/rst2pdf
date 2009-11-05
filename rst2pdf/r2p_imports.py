c612f89c969802c4927c13c151c6f97973150dba
pmaupin <pmaupin@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-11-05 15:25:59 +0000

    Removed check code from r2p_imports -- just do imports

--- rst2pdf/r2p_imports.py
+++ rst2pdf/r2p_imports.py
@@ -10,12 +10,9 @@
 We maintain imports of other packages in one place.  This gives us these
 advantages:
     1) Slight efficiency gain
-    2) Notifications of symbol interference
-    3) Better control of optional imports
-    4) Just type 'r2p_imports.py xxx' to figure out where the heck xxx came from!!!
+    2) Centralized exception handling for optional imports
 '''
 
-unconditionalimports = '''
 import sys
 import os
 import tempfile
@@ -51,49 +48,6 @@ from roman import toRoman
 from xml.sax.saxutils import unescape, escape
 
 from cStringIO import StringIO
-'''
-
-def loadunconditional():
-    def setpaths():
-        import os
-        thisdir = os.path.dirname(__file__)
-        scriptfile = os.path.join(thisdir, '..', 'bin', 'rst2pdf')
-        f = open(scriptfile, 'rb')
-        data = f.read()
-        f.close()
-        data = data.split('import rst2pdf')[0]
-        exec data in {}
-    if __name__ == '__main__':
-        setpaths()
-    importinfo = {}
-    badimports = []
-    globaldir = {}
-    for i, line in enumerate(unconditionalimports.splitlines()):
-        line = line.split('#')[0].rstrip()
-        if not line:
-            continue
-        newdir = {}
-        try:
-            exec line in newdir
-        except:
-            print line
-            raise
-        line = '%s # %d' % (line, i)
-        for key, value in newdir.iteritems():
-            oldline = importinfo.setdefault(key, line)
-            if oldline is not line and value is not globaldir[key]:
-                badimports.append((key, oldline, line))
-        globaldir.update(newdir)
-
-    if badimports:
-        badimports = ['%s defined by "%s" and "%s' % x for x in badimports]
-        badimports = '\n        '.join(badimports)
-        raise SystemExit('\nError: conflicting imported symbols:\n\n%s\n' %
-                            badimports)
-
-    return globaldir, importinfo
-
-_unconditional, _unconditional_info = loadunconditional()
 
 try:
     from PIL import Image as PILImage
@@ -150,54 +104,7 @@ except ImportError:
         def full():
             pass
 
-def dumpinfo(varnames):
-    import inspect
-    print
-    g = globals()
-    for name in varnames:
-        if name not in g:
-            print '%s not defined globally' % repr(name)
-            continue
-        value = g[name]
-        info = '%s (value %s)' % (repr(name), repr(value))
-        line = _unconditional_info.get(name)
-        if line is not None:
-            print '%s imported with %s' % (
-                    info, repr(line.split('#')[0].rstrip()))
-        else:
-            print '%s imported or defined explicitly (see source code)'
-        try:
-            sourcef = inspect.getfile(value)
-        except TypeError:
-            pass
-        else:
-            if sourcef.endswith('.pyc'):
-                sourcef = sourcef[:-1]
-            print '      defined in %s' % sourcef
-    print
-
-def checkconditional():
-    ignore = set('__builtins__ Paragraph ParagraphStyle getSampleStyleSheet'.split())
-    globaldict = globals()
-    unconditional = _unconditional
-    globalset = set(globaldict)
-    unconditionalset = set(unconditional)
-    common = (globalset & unconditionalset) - ignore
-    common = sorted(x for x in common if globaldict[x] is not unconditional[x])
-    if common:
-        raise SystemExit('imports redefined conditionally: %s' % ', '.join(common))
-    globaldict.update(unconditional)
-checkconditional()
-
 #def escape (x,y):
 #    "Dummy escape function to test for excessive escaping"
 #    return x
 
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print "\nAll imports are fine.  Type r2p_imports.py <name> to see where a name comes from.\n"
-    else:
-        dumpinfo(sys.argv[1:])
-
-del unconditionalimports, loadunconditional, _unconditional, _unconditional_info
-del dumpinfo, checkconditional
