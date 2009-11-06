40d23d3a17f5c919dc9be0575242f05249f59e6c
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-11-06 17:37:54 +0000

    Renamed so nosetests finds it automatically

--- rst2pdf/tests/test.py
+++ rst2pdf/tests/test.py
@@ -6,16 +6,14 @@ import sys
 
 class RunTest:
     def __init__(self,f):
-        sys.stdout.write(f)
-        sys.stdout.flush()
         self.description = f
         
     def __call__(self,f):
         key, errcode = run_single_test(self.description)
-        assert key == 'good', '%s is not good'%f
+        assert key == 'good', '%s is not good: %s'%(f,key)
 
 def test():
     testfiles = globjoin(PathInfo.inpdir, '*.txt')
     results = {}
-    for fname in testfiles[-5:]:
+    for fname in testfiles:
         yield RunTest(fname), fname
