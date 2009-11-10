02446c41054318d74e0d41348ecdbbee1598e2e4
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2009-11-10 23:20:16 +0000

    Skip tests that can't pass (no good md5), mostly useful for hudson

--- rst2pdf/tests/test.py
+++ rst2pdf/tests/test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from autotest import PathInfo, globjoin
+from autotest import MD5Info, PathInfo, globjoin
 from autotest import run_single
 
 import sys, os
@@ -8,13 +8,28 @@ import nose.plugins.skip
 
 class RunTest:
     def __init__(self,f):
-        self.description = os.path.basename(f)
-        
+        basename = os.path.basename(f)
+        self.description = basename 
+        mprefix = os.path.join(PathInfo.md5dir, basename)[:-4]
+        md5file = mprefix + '.json'
+        info=MD5Info()
+        if os.path.exists(md5file):
+            f = open(md5file, 'rb')
+            exec f in info
+            f.close()
+        if info.good_md5 in [[],['sentinel']]:
+            # This is an open issue or something that can't be checked automatically
+            self.skip=True
+        else:
+            self.skip=False
     def __call__(self,f):
-        key, errcode = run_single(f)
-	if key in ['incomplete']:
+        if self.skip:
             raise nose.plugins.skip.SkipTest
-        assert key == 'good', '%s is not good: %s'%(f,key)
+        else:
+            key, errcode = run_single(f)
+            if key in ['incomplete']:
+                raise nose.plugins.skip.SkipTest
+            assert key == 'good', '%s is not good: %s'%(f,key)
 
 def test():
     testfiles = globjoin(PathInfo.inpdir, '*.txt')
