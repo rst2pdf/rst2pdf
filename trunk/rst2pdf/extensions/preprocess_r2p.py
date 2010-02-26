825c80b483e1767e355d2fb034a5ee09735b44ed
pmaupin <pmaupin@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2010-02-26 20:20:45 +0000

    Added an extension module to do some preprocessing before we pass the source on to docutils.  Enables some cleaner syntax for common operations, as well as stylesheets embedded in the restructured text source.

---
+++ trunk/rst2pdf/extensions/preprocess_r2p.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*-
+
+# An extension module for rst2pdf
+# Copyright 2010, Patrick Maupin
+# See LICENSE.txt for licensing terms
+
+'''
+preprocess is a rst2pdf extension module (invoked by -e preprocess
+on the rst2pdf command line.
+
+It preprocesses the source text file before handing it to docutils.
+
+This module serves two purposes:
+
+1) It demonstrates the technique and can be a starting point for similar
+user-written processing modules; and
+
+2) It provides a simplified syntax for documents which are targeted only
+at rst2pdf, rather than docutils in general.
+
+The design goal of "base rst2pdf" is to be completely compatible with
+docutils, such that a file which works as a PDF can also work as HTML,
+etc.
+
+Unfortunately, base docutils is a slow-moving target, and does not
+make this easy.  For example, SVG images do not work properly with
+the HTML backend unless you install a patch, and docutils has no
+concept of page breaks or additional vertical space (other than
+the <hr>).
+
+So, while it would be nice to have documents that render perfectly
+with any backend, this goal is hard to achieve for some documents,
+and once you are restricted to a particular transformation type,
+then you might as well have a slightly nicer syntax for your source
+document.
+
+-----------------------------------------------------------------
+
+Preprocessor extensions:
+
+All current extensions except style occupy a single line in the
+source file.
+
+.. contents::   default is changed to "Table of Contents" if no text is given
+
+.. include::    Processes the include file as well.
+
+.. page::       is translated into a raw PageBreak
+
+.. space::      is translated into a raw Spacer.  If only one number given, is
+                used for vertical
+
+.. style::      Allows you to create in-line stylesheets.
+                (If you wish them to be in YAML format, you must give
+                -e yaml before -e preprocess on the command line.)
+
+.. widths::     creates a new table style (based on table or the first
+                non-numeric token) and creates a class using that style for
+                the next table.  Allows you to set the widths for the table,
+                using percentages.
+
+SingleWordAtLeftColumn   If this is surrounded by blank lines, the singleword
+                         style is applied to the word.  This is a workaround
+                         for the broken interaction between docutils subtitles
+                         and bibliographic metadata.  Who wants a subtitle
+                         inside the TOC?
+
+-----------------------------------------------------------------
+
+Preprocessor operation:
+
+The preprocessor generates a file that has the same name as the source
+file, with .build_temp. embedded in the name, and then passes that
+file to the restructured text parser.
+
+This file is left on the disk after operation, because any error
+messages from docutils will refer to line numbers in it, rather than
+in the original source.
+
+'''
+
+import os
+import re
+
+from rst2pdf.opt_imports import json_loads
+from rst2pdf.log import log
+
+class DummyFile(str):
+    def read(self):
+        return self
+
+class Preprocess(object):
+    singleword = r'(?:\n\s*\n)([A-Za-z]+\s*\n)(?:\s*\n)'
+    keywords = set('page contents space widths style include'.split())
+    comment = r'^(\.\.\s+(?:%s)\:\:.*\n)' % '|'.join(keywords)
+    expression = '(?:%s)' % '|'.join([singleword, comment])
+    splitter = re.compile(expression, re.MULTILINE).split
+
+    def __init__(self, sourcef, incfile=False):
+        name = sourcef.name
+        source = sourcef.read().replace('\r\n', '\n').replace('\r', '\n')
+
+        if incfile:
+            try:
+                self.styles = json_loads(source)
+            except:
+                pass
+            else:
+                self.styles['styles'] = dict(self.styles['styles'])
+                self.changed = True
+                self.keep = False
+                return
+
+        sourcef = DummyFile(source)
+        sourcef.name = name
+        self.source = source = [x for x in self.splitter(source) if x]
+        self.result = result = []
+        self.styles = {}
+        self.widthcount = 0
+        self.changed = False
+
+        source.reverse()
+        while source:
+            chunk = source.pop()
+            result.append(chunk)
+
+            # Only process single lines
+            if not chunk.endswith('\n') or chunk.index('\n') != len(chunk)-1:
+                continue
+            tokens = chunk.split()
+            if len(tokens) >= 2 and tokens[0] == '..' and tokens[1].endswith('::'):
+                keyword = tokens[1][:-2]
+                if keyword not in self.keywords:
+                    continue
+                chunk = chunk.split('::', 1)[1]
+            elif len(tokens) == 1 and chunk[0].isalpha() and tokens[0].isalpha():
+                keyword = 'single'
+                chunk = tokens[0]
+            else:
+                continue
+
+            result.pop()
+            getattr(self, 'handle_'+keyword)(chunk.strip())
+
+        if self.changed:
+            result.append('')
+            result = DummyFile('\n'.join(result))
+            result.name = name + '.build_temp'
+            self.keep = keep = len(result.strip())
+            if keep:
+                f = open(result.name, 'wb')
+                f.write(result)
+                f.close()
+            self.result = result
+        else:
+            self.result = sourcef
+
+    def handle_include(self, fname):
+        inc = Preprocess(open(fname, 'rb'), True)
+        self.styles.update(inc.styles)
+        if inc.changed:
+            self.changed = True
+            if not inc.keep:
+                return
+            fname = inc.result.fname
+        self.result.extend(['', '.. include:: ' + fname, ''])
+
+    def handle_single(self, word):
+        self.changed = True
+        self.result.extend(['', '.. class:: singleword', '', word, ''])
+
+    def handle_page(self, chunk):
+        self.changed = True
+        self.result.extend(['', '.. raw:: pdf', '',
+                    '    PageBreak ' + chunk, ''])
+
+    def handle_contents(self, chunk):
+        if not chunk:
+            chunk = 'Table of Contents'
+            self.changed = True
+        self.result.extend(['', '.. contents:: ' + chunk, ''])
+
+    def handle_space(self, chunk):
+        self.changed = True
+        if len(chunk.replace(',', ' ').split()) == 1:
+            chunk = '0 ' + chunk
+        self.result.extend(['', '.. raw:: pdf', '',
+                    '    Spacer ' + chunk, ''])
+
+    def handle_widths(self, chunk):
+        self.changed = True
+        chunk = chunk.replace(',', ' ').replace('%', ' ').split()
+        if not chunk:
+            log.error('no widths specified in .. widths ::')
+            return
+        parent = chunk[0][0].isalpha() and chunk.pop(0) or 'table'
+        values = [float(x) for x in chunk]
+        total = sum(values)
+        values = [int(round(100 * x / total)) for x in values]
+        while 1:
+            total = sum(values)
+            if total > 100:
+                values[index(max(values))] -= 1
+            elif total < 100:
+                values[index(max(values))] += 1
+            else:
+                break
+
+        values = ['%s%%' % x for x in values]
+        self.widthcount += 1
+        stylename = 'embeddedtablewidth%d' % self.widthcount
+        self.styles.setdefault('styles', {})[stylename] = dict(parent=parent, colWidths=values)
+        self.result.extend(['', '.. class:: ' + stylename, ''])
+
+    def handle_style(self, chunk):
+        self.changed = True
+        if chunk:
+            log.error(".. styles:: does not recognize string %s" % repr(chunk))
+            return
+        source = self.source
+        data = source and source.pop().splitlines() or []
+        data.reverse()
+        mystyles = []
+        while data:
+            myline = data.pop().rstrip()
+            if not myline:
+                continue
+            if myline.lstrip() == myline:
+                data.append(myline)
+                break
+            mystyles.append(myline)
+        data.reverse()
+        data.append('')
+        source.append('\n'.join(data))
+        if not mystyles:
+            log.error("Empty .. styles:: block found")
+        indent = min(len(x) - len(x.lstrip()) for x in mystyles)
+        mystyles = [x[indent:] for x in mystyles]
+        mystyles.append('')
+        mystyles = '\n'.join(mystyles)
+        try:
+            styles = json_loads(mystyles)
+            self.styles.setdefault('styles', {}).update(styles)
+        except ValueError, e: # Error parsing the JSON data
+                log.critical('Error parsing stylesheet "%s": %s'%\
+                    (mystyles, str(e)))
+
+class MyStyles(str):
+    def __new__(cls, styles):
+        self = str.__new__(cls, 'Embedded Preprocess Styles')
+        self.data = styles
+        return self
+    def __call__(self):
+        return self.data
+
+def install(createpdf, options):
+    data = Preprocess(options.infile)
+    options.infile = data.result
+    if data.styles:
+        options.style.append(MyStyles(data.styles))
