7aa729349a6e017fc8cab5b2b3de650e3ec74609
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2010-03-15 15:29:04 +0000

    Starting work on Issue 280

--- trunk/setup.py
+++ trunk/setup.py
@@ -6,7 +6,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.13'
+version = '0.14'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
