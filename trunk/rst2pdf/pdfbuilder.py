efca818c3f51fa24664bb69a17a4c07a44180399
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2010-03-18 19:32:20 +0000

    Fixes Issue 252

--- trunk/rst2pdf/pdfbuilder.py
+++ trunk/rst2pdf/pdfbuilder.py
@@ -776,8 +776,37 @@ def try_parse(src):
     else:
         return True
 
+def init_math(app):
+    """
+        This is a dummy math extension.
+        
+        It's a hack, but if you want math in a PDF via pdfbuilder, and don't want to
+        enable pngmath or jsmath, then enable this one.
+        
+        :copyright: Copyright 2007-2009 by the Sphinx team, see AUTHORS.
+        :license: BSD, see LICENSE for details.
+    """
+    from sphinx.errors import SphinxError
+    from sphinx.ext.mathbase import setup_math as mathbase_setup
+    class MathExtError(SphinxError):
+        category = 'Math extension error'
+
+
+    def html_visit_math(self, node):
+        self.body.append(node['latex'])
+        raise nodes.SkipNode
+
+    def html_visit_displaymath(self, node):
+        self.body.append(node['latex'])
+        raise nodes.SkipNode
+
+    mathbase_setup(app, (html_visit_math, None), (html_visit_displaymath, None))
+
 
 def setup(app):
+    #Init dummy math extension
+    init_math(app)
+    
     app.add_builder(PDFBuilder)
     # PDF options
     app.add_config_value('pdf_documents', [], None)
