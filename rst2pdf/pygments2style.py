4aa81c2b69084af113e1b74f24539ed21e352542
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2010-03-22 14:45:13 +0000

    Fixes Issue 297

--- rst2pdf/pygments2style.py
+++ rst2pdf/pygments2style.py
@@ -10,17 +10,26 @@ import dumpstyle
 from pygments.token import STANDARD_TYPES
 from pygments import styles as pstyles
 
+# First get a list of all possible classes
+classnames=set()
+for name in list(pstyles.get_all_styles()):
+    css=os.popen('pygmentize -S %s -f html'%name, 'r').read()
+    for line in css.splitlines():
+        line = line.strip()
+        sname = "pygments-" + line.split(' ')[0][1:]
+        classnames.add(sname)
 
 def css2rl(css):
     dstyles = {}
     # First create a dumb stylesheet
     for key in STANDARD_TYPES:
         dstyles["pygments-" + STANDARD_TYPES[key]] = {'parent': 'code'}
-
+    seenclassnames=set()
     styles = []
     for line in css.splitlines():
         line = line.strip()
         sname = "pygments-" + line.split(' ')[0][1:]
+        seenclassnames.add(sname)
         style = dstyles.get(sname, {'parent': 'code'})
         options = line.split('{')[1].split('}')[0].split(';')
         for option in options:
@@ -46,10 +55,18 @@ def css2rl(css):
                 else:
                     style['fontName'] = 'stdMonoItalic'
         if style.get('textColor', None) is None:
-		style['textColor']='black'
+            style['textColor']='black'
         styles.append([sname, style])
 
+    # Now add default styles for all unseen class names
+    for sname in classnames-seenclassnames:
+        style = dstyles.get(sname, {'parent': 'code'})
+        style['textColor']='black'
+        styles.append([sname, style])
+        
     return dumpstyle.dumps({'styles': styles})
+
+    
 
 for name in list(pstyles.get_all_styles()):
     css=os.popen('pygmentize -S %s -f html'%name, 'r').read()
