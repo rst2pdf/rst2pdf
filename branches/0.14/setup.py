f50611b707f3597b1a82bf872262ace15fab9208
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2010-03-25 00:05:05 +0000

    Preparing 0.14.1

--- branches/0.14/setup.py
+++ branches/0.14/setup.py
@@ -6,7 +6,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.14'
+version = '0.14.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
