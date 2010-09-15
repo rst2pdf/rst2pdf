07afa3959c01ef274e2d26103c6506faf7ec7172
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2010-09-15 17:58:49 +0000

    Make numbered links work better with repeated section titles that have alternate labels.

--- rst2pdf/sectnumlinks.py
+++ rst2pdf/sectnumlinks.py
@@ -7,7 +7,7 @@ class SectNumFolder(docutils.nodes.SparseNodeVisitor):
 
     def visit_generated(self, node):
         for i in node.parent.parent['ids']:
-            self.sectnums[i]=node.astext().strip()
+            self.sectnums[i]=node.parent.astext().replace(u'\xa0\xa0\xa0',' ')
 
 class SectRefExpander(docutils.nodes.SparseNodeVisitor):
     def __init__(self, document, sectnums):
@@ -16,4 +16,4 @@ class SectRefExpander(docutils.nodes.SparseNodeVisitor):
 
     def visit_reference(self, node):
         if node.get('refid', None) in self.sectnums:
-            node.children.insert(0,docutils.nodes.Text('%s '%self.sectnums[node.get('refid')]))
+            node.children=[docutils.nodes.Text('%s '%self.sectnums[node.get('refid')])]
