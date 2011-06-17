082d7f79b2be21b4240fbda11454e71ac7b15b02
roberto.alsina <roberto.alsina@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2011-06-17 16:17:39 +0000

    Fixes Issue 404

--- rst2pdf/extensions/plantuml.py
+++ rst2pdf/extensions/plantuml.py
@@ -14,6 +14,9 @@ from docutils import nodes
 from docutils.parsers import rst
 from docutils.parsers.rst import directives
 import rst2pdf.genelements as genelements
+from rst2pdf.image import MyImage
+import tempfile
+import subprocess
 
 class plantuml(nodes.General, nodes.Element):
     pass
@@ -29,14 +32,20 @@ class UmlDirective(rst.Directive):
 
            Alice -> Bob: Hello
            Alice <- Bob: Hi
+
+
+    You can use a :format: option to change between SVG and PNG diagrams, however,
+    the SVG plantuml generates doesn't look very good to me.
     """
     has_content = True
     option_spec = {'alt': directives.unchanged}
+    option_spec = {'format': directives.unchanged}
 
     def run(self):
         node = plantuml()
         node['uml'] = '\n'.join(self.content)
         node['alt'] = self.options.get('alt', None)
+        node['format'] = self.options.get('format', 'png')
         return [node]
 
 
@@ -44,7 +53,25 @@ class UMLHandler(genelements.NodeHandler, plantuml):
     """Class to handle UML nodes"""
 
     def gather_elements(self, client, node, style):
-        print "UML"
-        return []
+        # Create image calling plantuml
+        tfile = tempfile.NamedTemporaryFile(dir='.', delete=False, suffix='.'+node['format'])
+        args = 'plantuml -pipe -charset utf-8'
+        if node['format'].lower() == 'svg':
+            args+=' -tsvg'
+        client.to_unlink.append(tfile.name)
+        try:
+            p = subprocess.Popen(args.split(), stdout=tfile,
+                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        except OSError, err:
+            if err.errno != ENOENT:
+                raise
+            raise PlantUmlError('plantuml command %r cannot be run'
+                                % self.builder.config.plantuml)
+        serr = p.communicate(node['uml'].encode('utf-8'))[1]
+        if p.returncode != 0:
+            raise PlantUmlError('error while running plantuml\n\n' + serr)
+        
+        # Add Image node with the right image
+        return [MyImage(tfile.name, client=client)]
 
 directives.register_directive("uml", UmlDirective)
