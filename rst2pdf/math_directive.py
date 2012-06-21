59dba57ee318499feb98cf48520e18c0fac033b3
roberto.alsina@gmail.com <roberto.alsina@gmail.com@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2012-06-21 17:36:19 +0000

    Math patches by Paul Nation and Robert Johansson

--- rst2pdf/math_directive.py
+++ rst2pdf/math_directive.py
@@ -18,6 +18,8 @@ if 'Directive' in rst.__dict__:
         final_argument_whitespace = True
         option_spec = {
             'label': directives.unchanged,
+            'fontsize': directives.nonnegative_int,
+            'color': directives.unchanged,
             'nowrap': directives.flag,
         }
 
@@ -29,8 +31,12 @@ if 'Directive' in rst.__dict__:
             if self.arguments and self.arguments[0] and not latex:
                 latex = self.arguments[0]
             label=self.options.get('label', None)
+            fontsize=self.options.get('fontsize', None)
+            color=self.options.get('color', 'black')
             return [math_node(latex=latex,
                               label=label,
+                              fontsize=fontsize,
+                              color=color,
                               rawsource=''.join(self.content))]
 
         def __repr__(self):
@@ -50,10 +56,12 @@ directives.register_directive('math', Math)
 class math_node(General, Inline, Element):
     children = ()
 
-    def __init__(self, rawsource='', label=None, *children, **attributes):
+    def __init__(self, rawsource='', label=None, fontsize=12, color='black', *children, **attributes):
         self.rawsource = rawsource
         self.math_data = attributes['latex']
         self.label = label
+        self.fontsize = fontsize
+        self.color = color
         Element.__init__(self, rawsource, *children, **attributes)
 
 
@@ -76,10 +84,14 @@ roles.register_local_role('eq', eq_role)
 
 class HandleMath(basenodehandler.NodeHandler, math_node):
     def gather_elements(self, client, node, style):
-        return [math_flowable.Math(node.math_data,node.label)]
+        return [math_flowable.Math(node.math_data,node.label,node.fontsize,node.color)]
 
     def get_text(self, client, node, replaceEnt):
-        mf = math_flowable.Math(node.math_data)
+        #get style for current node
+        sty=client.styles.styleForNode(node)
+        node_fontsize=sty.fontSize
+        node_color='#'+sty.textColor.hexval()[2:]
+        mf = math_flowable.Math(node.math_data,label=node.label,fontsize=node_fontsize,color=node_color)
         w, h = mf.wrap(0, 0)
         descent = mf.descent()
         img = mf.genImage()
