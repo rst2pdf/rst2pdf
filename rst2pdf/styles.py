f698f383ca4772b9d70c4c6df4dae476592717b3
roberto.alsina@gmail.com <roberto.alsina@gmail.com@3777fadb-0f44-0410-9e7f-9d8fa6171d72>
2012-10-19 18:17:42 +0000

    Fixes Issue 474

--- rst2pdf/styles.py
+++ rst2pdf/styles.py
@@ -524,7 +524,7 @@ class StyleSheet(object):
         # Make stdFont the basefont, for Issue 65
         reportlab.rl_config.canvas_basefontname = self['base'].fontName
         # Make stdFont the default font for table cell styles (Issue 65)
-        reportlab.platypus.tables.CellStyle1.fontname=self['base'].fontName
+        reportlab.platypus.tables.CellStyle.fontname=self['base'].fontName
 
 
     def __getitem__(self, key):
