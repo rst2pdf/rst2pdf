243d58a8f238b0bdde94f351a919e24ccd4ba43e
David Townshend <david.townshend@aurecongroup.com>
2015-06-09 13:18:38 +0200

    Make the compare_pdf.sh utility more useful

--- rst2pdf/tests/compare_pdf.sh
+++ rst2pdf/tests/compare_pdf.sh
@@ -2,27 +2,50 @@
 
 # Given two PDF files in the command line, create a visual diff of them in another PDF
 
-f1=$1
-f2=$2
-flag=0
+function compare_pdfs {
+  f1=$PWD/$1
+  f2=$PWD/$2
+  flag=0
 
-tmpdir=/tmp/comppdf-$$ 
-mkdir $tmpdir
-convert -density 96x96 $f1 $tmpdir/page.png
-convert -density 96x96 $f2 $tmpdir/bpage.png
-pushd $tmpdir >/dev/null 2>&1
+  tmpdir=/tmp/comppdf-$$/$1
+  mkdir -p $tmpdir
+  convert -density 96x96 $f1 $tmpdir/page.png
+  convert -density 96x96 $f2 $tmpdir/bpage.png
 
-for page in page*.png
-do
-    result=`compare -metric PSNR $page b$page diff$page 2>&1`
-    if [ "$result" = "inf" ]
-    then
-            #echo "$page is OK"
-            true
-    else
-            echo "$page has ERRORs, see $tmpdir/diff$page"
-	    flag=1
-    fi    
+  pages=$(find $tmpdir -name page*.png | wc -l)
+  bpages=$(find $tmpdir -name bpage*.png | wc -l)
+  if [[ $pages != $bpages ]]; then
+    echo Page count mismatch \($pages != $bpages\)
+  else
+    pushd $tmpdir >/dev/null 2>&1
+    for page in page*.png
+    do
+        result=`compare -metric PSNR $page b$page diff$page 2>&1`
+        if [ "$result" = "inf" ]
+        then
+          echo "$page is OK"
+        else
+          echo "$page has ERRORs, see $tmpdir/diff$page"
+          display $tmpdir/diff$page
+    	    flag=1
+        fi
+    done
+    popd >/dev/null 2>&1
+    if [[ $flag == 1 ]]; then
+      read -p "Replace expected output? " replace;
+      if [[ $replace == "y" ]]; then
+        cp -fv $f1 $f2;
+      elif [[ $replace == "q" ]]; then
+        exit 1;
+      fi
+    fi
+  fi
+}
+
+for fname in $(find expected_output -iname *.pdf -printf "%f\n"); do
+  if [[ -e output/$fname ]]; then
+    compare_pdfs output/$fname expected_output/$fname
+  else
+    echo "Skipping $fname"
+  fi
 done
-popd >/dev/null 2>&1
-exit $flag
