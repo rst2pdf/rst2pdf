4eeafbd40a768f3428a6cea7929678a9232dcb01
Erich Seifert <dev@erichseifert.de>
2016-04-23 11:34:38 +0200

    Used quoted attributes for <img>

--- rst2pdf/sphinxnodes.py
+++ rst2pdf/sphinxnodes.py
@@ -194,7 +194,7 @@ class HandleSphinxMath(SphinxHandler, mathbase.math, mathbase.displaymath):
         descent = mf.descent()
         img = mf.genImage()
         client.to_unlink.append(img)
-        return '<img src="%s" width=%f height=%f valign=%f/>' % (
+        return '<img src="%s" width="%f" height="%f" valign="%f"/>' % (
             img, w, h, -descent)
 
 class HandleSphinxEq(SphinxHandler, mathbase.eqref):
