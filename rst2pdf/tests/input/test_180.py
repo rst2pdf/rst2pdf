d0266d17d87766dd06027d10292660f99b24d116
oesteban <code@oscaresteban.es>
2016-08-19 16:33:06 -0700

    remove wordaxe

--- rst2pdf/tests/input/test_180.py
+++ rst2pdf/tests/input/test_180.py
@@ -4,14 +4,11 @@ from reportlab.platypus.paragraph import Paragraph
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.colors import Color
 from reportlab.platypus.flowables import _listWrapOn, _FUZZ
-from wordaxe.rl.NewParagraph import Paragraph
-from wordaxe.rl.styles import ParagraphStyle, getSampleStyleSheet
-
 
 def go():
     styles = getSampleStyleSheet()
     style=styles['Normal']
-    
+
     p1 = Paragraph('This is a paragraph', style )
     print p1.wrap(500,701)
     print p1._cache['avail']
