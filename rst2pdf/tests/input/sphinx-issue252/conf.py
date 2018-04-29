7ced47de09fd89c1dabcbbdca9351acfdabef1d0
Rob Allen <rob@akrabat.com>
2018-04-29 17:22:13 +0100

    Add good hash for sphinx-issue252
    Note that the rst2pdf.pdfbuilder extension needs to be LAST as it
    registers its own math extension

--- rst2pdf/tests/input/sphinx-issue252/conf.py
+++ rst2pdf/tests/input/sphinx-issue252/conf.py
@@ -23,9 +23,9 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 
-# It's probably a good idea to use pdfbuilder BEFORE pngmath
+# It's probably a good idea to ensure that pdfbuilder is LAST
 
-extensions = ['rst2pdf.pdfbuilder','sphinx.ext.pngmath']
+extensions = ['sphinx.ext.imgmath','rst2pdf.pdfbuilder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
