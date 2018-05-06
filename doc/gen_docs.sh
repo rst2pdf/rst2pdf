b7611bfdbf2b5eb5e43ded37e31d7cfaeb85b2ff
Rob Allen <rob@akrabat.com>
2018-05-06 12:19:48 +0100

    Update gen_docs.sh
    The source files now have a `.rst` extension.

--- doc/gen_docs.sh
+++ doc/gen_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../bin/rst2pdf manual.txt -o manual.pdf -s manual.style -b1
-rst2man rst2pdf.txt rst2pdf.1
+rst2pdf manual.rst -o manual.pdf -s manual.style -b1
+rst2man rst2pdf.rst rst2pdf.1
