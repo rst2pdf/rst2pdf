3c7a2d150f425b1434b275cdd4c3df30a7cf6312
Lorna Jane Mitchell <lorna@lornajane.net>
2018-06-24 21:45:11 +0100

    An extended rst2html to parse the manual, from ralsina

---
+++ doc/rst2html-manual.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# $Id: rst2html.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing HTML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.parsers.rst import directives
+import rst2pdf.pygments_code_block_directive
+directives.register_directive('code-block', rst2pdf.pygments_code_block_directive.code_block_directive)
+
+description = ('Generates (X)HTML documents from standalone reStructuredText '
+               'sources.  ' + default_description)
+
+publish_cmdline(writer_name='html', description=description)
