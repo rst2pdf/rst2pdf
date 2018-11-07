6dd4cd82d8e7bb496f939a7811fd29d4697735e0
Roberto Alsina <ralsina@medallia.com>
2018-11-07 12:08:18 -0300

    Fix handling of renamed links (Issue #569)

--- rst2pdf/genpdftext.py
+++ rst2pdf/genpdftext.py
@@ -222,9 +222,13 @@ class HandleTarget(NodeHandler, docutils.nodes.target):
 
     def get_pre_post(self, client, node, replaceEnt):
         pre = ''
-        if node['ids'][0] not in client.targets:
-            pre = u'<a name="%s"/>' % node['ids'][0]
-            client.targets.append(node['ids'][0])
+        if node['ids']:
+            if node['ids'][0] not in client.targets:
+                pre = u'<a name="%s"/>' % node['ids'][0]
+                client.targets.append(node['ids'][0])
+        else:
+            pre = u'<a name="%s"/>' % node['refuri']
+            client.targets.append(node['refuri'])
         return pre, ''
 
 class HandleInline(NodeHandler, docutils.nodes.inline):
