080aa9222a7a34c5794605675f34b7d72db7cc01
Roberto Alsina <ralsina@medallia.com>
2018-11-28 14:28:30 -0300

    follow suggestions from code review

--- rst2pdf/tests/check_artifacts.py
+++ rst2pdf/tests/check_artifacts.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 
+"""Hacky script to validate testing artifacts.
+
+* If there is a reference artifact that doesn't have a known-good checksum
+  it will print 'BAD ARTIFACT'.
+
+* If there is a testing artifact in output/ that has a known-good checksum
+  for a test that has no known reference artifact:
+
+  It will print 'GOOD ARTIFACT', and then copy that file into reference/
+"""
+
 import glob
 import shutil
 
 from autotest import checkmd5
+
+
 
 # First, let's check that the existing artifacts are "good"
 
@@ -19,10 +32,10 @@ for artifact in artifacts:
 
 for new_artifact in glob.glob('output/*.pdf'):
     artifact = new_artifact.replace('output/', 'reference/')
-    md5_path = artifact.replace('.pdf', '.json').replace('reference/', 'md5/')
     if artifact in artifacts:
         continue  # We already have an artifact for this test
     else:
+        md5_path = artifact.replace('.pdf', '.json').replace('reference/', 'md5/')
         res, _ = checkmd5(new_artifact, md5_path, [], False)
         if res == 'good':
             print 'GOOD ARTIFACT: %s ' % res, new_artifact
