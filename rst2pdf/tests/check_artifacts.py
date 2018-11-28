2e0a2e7ff03a8c9836e77a155460d52ca2e7541e
Roberto Alsina <ralsina@medallia.com>
2018-11-28 12:48:46 -0300

    hacky script to validate artifacts

---
+++ rst2pdf/tests/check_artifacts.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import glob
+import shutil
+
+from autotest import checkmd5
+
+# First, let's check that the existing artifacts are "good"
+
+artifacts = glob.glob('reference/*.pdf')
+
+for artifact in artifacts:
+    md5_path = artifact.replace('.pdf', '.json').replace('reference/', 'md5/')
+    res, _ = checkmd5(artifact, md5_path, [], False)
+    if res == 'good':
+        pass
+    else:
+        print 'BAD ARTIFACT: %s ' % res, artifact
+
+for new_artifact in glob.glob('output/*.pdf'):
+    artifact = new_artifact.replace('output/', 'reference/')
+    md5_path = artifact.replace('.pdf', '.json').replace('reference/', 'md5/')
+    if artifact in artifacts:
+        continue  # We already have an artifact for this test
+    else:
+        res, _ = checkmd5(new_artifact, md5_path, [], False)
+        if res == 'good':
+            print 'GOOD ARTIFACT: %s ' % res, new_artifact
+            shutil.copy(new_artifact, artifact)
