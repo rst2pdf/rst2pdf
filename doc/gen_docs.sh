82d089f3cdacfd1fcc431f44c286a66326450f8a
Lorna Jane Mitchell <lorna@lornajane.net>
2018-12-16 21:03:01 +0000

    Add missing image, move outputs to their own directories

--- doc/gen_docs.sh
+++ doc/gen_docs.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rst2pdf manual.rst --custom-cover=assets/cover.tmpl -o output/manual.pdf -s assets/manual.style -b1
+rst2pdf manual.rst --custom-cover=assets/cover.tmpl -o output/pdf/manual.pdf -s assets/manual.style -b1
 rst2man rst2pdf.rst output/rst2pdf.1
 
 # set PYTHONPATH so we use the current contents of this repo, rather than our installed rst2pdf
-PYTHONPATH=../ python rst2html-manual.py --stylesheet=assets/manual.css manual.rst output/manual.html
+PYTHONPATH=../ python rst2html-manual.py --stylesheet=assets/manual.css manual.rst output/html/manual.html
+cp assets/biohazard.png output/html/assets/
