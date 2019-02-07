3c1c2ddf059f27d3f2fd3367aab082bd180af1e8
Roberto Alsina <roberto.alsina@gmail.com>
2019-02-07 13:47:11 -0300

    Initial support for docutils.nodes.math_block node

--- rst2pdf/genelements.py
+++ rst2pdf/genelements.py
@@ -59,6 +59,7 @@ from .flowables import Table, DelayedTable, SplitTable, Heading, \
               Separation, BoxedContainer, BoundByWidth, \
               MyPageBreak, Reference, tablepadding, OddEven, \
               XPreformatted
+import math_flowable
 
 from .opt_imports import wordaxe, Paragraph, ParagraphStyle
 
@@ -948,3 +949,21 @@ class HandleAdmonition(NodeHandler, docutils.nodes.attention,
                                 colWidths=[0,None]),
                                 MySpacer(0,st.spaceAfter)]
         return node.elements
+
+
+class HandleMath(NodeHandler, docutils.nodes.math_block):
+    def gather_elements(self, client, node, style):
+        return [math_flowable.Math(node.astext(), "", style.fontSize, style.textColor.rgb())]
+
+    def get_text(self, client, node, replaceEnt):
+        #get style for current node
+        sty=client.styles.styleForNode(node)
+        node_fontsize=sty.fontSize
+        node_color='#'+sty.textColor.hexval()[2:]
+        mf = math_flowable.Math(node.astext(),label=node.label,fontsize=node_fontsize,color=node_color)
+        w, h = mf.wrap(0, 0)
+        descent = mf.descent()
+        img = mf.genImage()
+        client.to_unlink.append(img)
+        return '<img src="%s" width="%f" height="%f" valign="%f"/>' % (
+            img, w, h, -descent)
