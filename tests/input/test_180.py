832435a4f7603c1b66d35d3e2478953715eaf84e
Norman Lorrain <normanlorrain@gmail.com>
2020-01-03 19:32:36 -0700

    Moved tests directory outsite of package, per pytest recommendation.

--- tests/input/test_180.py
+++ tests/input/test_180.py
@@ -10,13 +10,14 @@ from wordaxe.rl.styles import ParagraphStyle, getSampleStyleSheet
 
 def go():
     styles = getSampleStyleSheet()
-    style=styles['Normal']
+    style = styles["Normal"]
 
-    p1 = Paragraph('This is a paragraph', style )
-    print p1.wrap(500,701)
-    print p1._cache['avail']
-    print len(p1.split(500,701))
-    print p1.wrap(500,700)
-    print len(p1.split(500,700))
+    p1 = Paragraph("This is a paragraph", style)
+    print(p1.wrap(500, 701))
+    print(p1._cache["avail"])
+    print(len(p1.split(500, 701)))
+    print(p1.wrap(500, 700))
+    print(len(p1.split(500, 700)))
+
 
 go()
