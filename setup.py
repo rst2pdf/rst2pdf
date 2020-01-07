4fd5d5935d2734e85b57d6d58024f294f5322627
Norman Lorrain <normanlorrain@gmail.com>
2020-01-06 18:04:07 -0700

    Undoing the auto format to keep changes to a minimum.

--- setup.py
+++ setup.py
@@ -5,55 +5,51 @@ import sys
 
 from setuptools import find_packages, setup
 
-version = "0.96"
-
+version = '0.96'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-
-long_description = read("README.rst")
+long_description = read('README.rst')
 
 install_requires = [
-    "docutils",
-    "jinja2",
-    "pdfrw",
-    "pygments",
-    "reportlab",
-    "setuptools",
-    "six",
-    "smartypants",
-]
+        'docutils',
+        'jinja2',
+        'pdfrw',
+        'pygments',
+        'reportlab',
+        'setuptools',
+        'six',
+        'smartypants',
+        ]
 
 try:
     import json
 except ImportError:
-    install_requires.append("simplejson")
+    install_requires.append('simplejson')
 
-tests_require = ["pyPdf2"]
-sphinx_require = ["sphinx"]
-hyphenation_require = ["wordaxe>=1.0"]
-svgsupport_require = ["svglib"]
-aafiguresupport_require = ["aafigure>=0.4"]
-mathsupport_require = ["matplotlib"]
-rawhtmlsupport_require = ["xhtml2pdf"]
+tests_require = ['pyPdf2']
+sphinx_require = ['sphinx']
+hyphenation_require = ['wordaxe>=1.0']
+svgsupport_require = ['svglib']
+aafiguresupport_require = ['aafigure>=0.4']
+mathsupport_require = ['matplotlib']
+rawhtmlsupport_require = ['xhtml2pdf']
 
 setup(
     name="rst2pdf",
     version=version,
-    python_requires=">=3.0",
-    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
-    package_data=dict(
-        rst2pdf=[
-            "styles/*.json",
-            "styles/*.style",
-            "images/*png",
-            "images/*jpg",
-            "templates/*tmpl",
-        ]
-    ),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    package_data=dict(rst2pdf=['styles/*.json',
+	'styles/*.style',
+	'images/*png',
+	'images/*jpg',
+	'templates/*tmpl'
+	]),
     include_package_data=True,
-    dependency_links=[],
+    dependency_links=[
+    ],
     install_requires=install_requires,
     tests_require=tests_require,
     extras_require=dict(
@@ -68,20 +64,20 @@ setup(
     # metadata for upload to PyPI
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Documentation",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing",
-        "Topic :: Utilities",
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+	'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
     ],
     author="rst2pdf maintainers",
     author_email="maintainers@rstpdf.org",
@@ -92,10 +88,10 @@ setup(
     keywords="restructured convert rst pdf docutils pygments reportlab",
     url="https://rst2pdf.org",
     project_urls={
-        "Bug Reports": "https://github.com/rst2pdf/rst2pdf/issues",
-        "Source": "https://github.com/rst2pdf/rst2pdf",
+        'Bug Reports': 'https://github.com/rst2pdf/rst2pdf/issues',
+        'Source': 'https://github.com/rst2pdf/rst2pdf',
     },
     download_url="https://github.com/rst2pdf/rst2pdf/releases",
-    entry_points={"console_scripts": ["rst2pdf = rst2pdf.createpdf:main"]},
-    test_suite="rst2pdf.tests.test_rst2pdf.test_suite",  # TODO: this needs to be updated
+    entry_points={'console_scripts': ['rst2pdf = rst2pdf.createpdf:main']},
+    test_suite='rst2pdf.tests.test_rst2pdf.test_suite',  # TODO: this needs to be updated
 )
