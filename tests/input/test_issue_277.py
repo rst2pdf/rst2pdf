1b575dd9a19de7b6acf6a715cddf821cb69e2f9c
Norman Lorrain <normanlorrain@gmail.com>
2020-01-15 19:37:07 -0700

    Restored test_issue_277.py (quotes)

--- tests/input/test_issue_277.py
+++ tests/input/test_issue_277.py
@@ -9,23 +9,18 @@ from reportlab.platypus.xpreformatted import *
 from reportlab.lib.styles import getSampleStyleSheet
 from copy import copy
 
-
 def go():
-    Story = []
-    styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate("tests/output/issue277.pdf")
-    ts = TableStyle()
-    knstyle = copy(styles["Normal"])
-    heading = Paragraph("A heading at the beginning of the document", knstyle)
-    heading.keepWithNext = True
-    print([["This is the content"] for x in range(12)])
-    content = Table(
-        [[Paragraph("This is the content", styles["Normal"])] for x in range(120)],
-        style=ts,
-    )
+        Story=[]
+        styles = getSampleStyleSheet()
+        doc = SimpleDocTemplate("issue277.pdf")
+        ts=TableStyle()
+        knstyle=copy(styles['Normal'])
+        heading=Paragraph('A heading at the beginning of the document',knstyle)
+        heading.keepWithNext=True
+        print([['This is the content'] for x in range(12)])
+        content= Table([[Paragraph('This is the content',styles['Normal'])] for x in range(120)], style=ts)
 
-    Story = [heading, content]
-    doc.build(Story)
-
+        Story=[heading,content]
+        doc.build(Story)
 
 go()
