d5bef5256878085b918d0680ac55f20dbf4a3b28
brendan <2bndy5@gmail.com>
2021-01-11 00:58:52 -0800

    add sphinx-versionmodified test

---
+++ rst2pdf/tests/input/sphinx-versionmodified/conf.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# -- General configuration -----------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be extensions
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = ['rst2pdf.pdfbuilder']
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = u'sphinxmarkup'
+copyright = u'2020'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = 'test'
+# The full version, including alpha/beta/rc tags.
+release = 'test'
+
+
+# -- Options for PDF output ----------------------------------------------------
+
+# Grouping the document tree into PDF files. List of tuples
+# (source start file, target name, title, author).
+pdf_documents = [('index', u'sphinx-versionmodified', u'My Project', u'Author Name')]
+
+# A comma-separated list of custom stylesheets. Example:
+pdf_stylesheets = ['sphinx']
+
+# Language to be used for hyphenation support
+pdf_language = "en_US"
+
+# If false, no index is generated.
+pdf_use_index = True
+
+# If false, no modindex is generated.
+pdf_use_modindex = False
+
+# If false, no coverpage is generated.
+pdf_use_coverpage = False
